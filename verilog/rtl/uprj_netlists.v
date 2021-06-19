5b47c7d3219d3dbf0efc16be98b8e3c61b211dfd
hamzashabbir517 <shabbirhamza517@gmail.com>
2021-06-19 09:44:00 +0500

    Update

--- verilog/rtl/uprj_netlists.v
+++ verilog/rtl/uprj_netlists.v
@@ -24,9 +24,14 @@
     `include "gl/user_project_wrapper.v"
     `include "gl/user_proj_example.v"
 `else
-    `include "user_project_wrapper.v"
-    `include "../gl/user_proj_example.v"
-    //`include "user_proj_example.v"
-    //`include "BrqRV_EB1/BrqRV_EB1.v"
     `include "BrqRV_EB1/sky130_sram_1kbyte_1rw1r_32x256_8.v"
+   
+   // for netlist verification
+    //`include "../gl/user_project_wrapper.v"
+    //`include "../gl/user_proj_example.v"
+   
+  // for rtl verification
+    `include "user_proj_example.v"
+    `include "BrqRV_EB1/BrqRV_EB1.v"
+    
 `endif
