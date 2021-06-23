66eb3042be46770213ef2d27b8677903abefa0f6
hamzashabbir517 <shabbirhamza517@gmail.com>
2021-06-23 13:05:10 +0100

    Update

--- verilog/dv/BrqRV_EB1/BrqRV_EB1_tb.v
+++ verilog/dv/BrqRV_EB1/BrqRV_EB1_tb.v
@@ -83,13 +83,13 @@ module BrqRV_EB1_tb();
             wait(mprj_io_0 == 28'd13);
             */
             // Observe Output pins [35:8] for multliplication_table
-            wait(mprj_io_0 == 28'd5);
+            /*wait(mprj_io_0 == 28'd5);
             wait(mprj_io_0 == 28'd10);
             wait(mprj_io_0 == 28'd15);
             wait(mprj_io_0 == 28'd20);
             wait(mprj_io_0 == 28'd25);
             wait(mprj_io_0 == 28'd30);
-            
+            */
             // Observe Output pins [35:8] for mean & Determinant
             //wait(mprj_io_0 == 28'd5);
             
@@ -100,9 +100,9 @@ module BrqRV_EB1_tb();
             //wait(mprj_io_0 == 28'd4889874);
             
             // Observe Output pins [35:8] for Queue 
-           // wait(mprj_io_0 == 28'd5);
-           // wait(mprj_io_0 == 28'd6);
-           // wait(mprj_io_0 == 28'd7);
+            wait(mprj_io_0 == 28'd5);
+            wait(mprj_io_0 == 28'd6);
+            wait(mprj_io_0 == 28'd7);
             
             // Observe Output pins [35:8] for perfect square
             //wait(mprj_io_0 == 28'd5);
@@ -125,7 +125,7 @@ module BrqRV_EB1_tb();
             //wait(mprj_io_0 == 28'd2);
             //wait(mprj_io_0 == 28'd1);
             //wait(mprj_io_0 == 28'd0);
-            $display("MPRJ-IO state = %d, at time = %0t ", mprj_io[35:8],$time);  
+            $display("MPRJ-IO state = %d", mprj_io[35:8]);  
 		
 		`ifdef GL
 	    	$display("Monitor: Test 1 Mega-Project IO (GL) Passed");
@@ -161,7 +161,7 @@ module BrqRV_EB1_tb();
 	end
 	
 	always @(mprj_io) begin
-		#1 $display("MPRJ-IO state = %d ", mprj_io[35:8]);
+		#1 $display("MPRJ-IO state = %d, at time = %0t  ", mprj_io[35:8], $time);
 	end
 	
 	wire flash_csb;
